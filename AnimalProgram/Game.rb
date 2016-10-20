b36805c3f87ef5bd42ebbdde834575c8cae1b4fb
Patrick M. Ryan <pmryan@us.ibm.com>
2016-10-20 13:54:22 -0400

    more refactoring

--- AnimalProgram/Game.rb
+++ AnimalProgram/Game.rb
@@ -4,16 +4,15 @@ require './AnimalDB.rb'
 require './GameState.rb'
 
 class Game
-  
   def initialize
     ##puts "initializing a Game instance"
     @adb = nil
   end
-  
+
   attr_accessor :adb, :state
-  
+
   def initializeGame
-    
+
     #puts "starting"
     self.adb=(AnimalDB.new())
     self.adb().initializeTree()
@@ -24,15 +23,15 @@ class Game
     aState.parent_node=(root)
     aState.current_node=(left)
     self.state=(aState)
-    
+
     #puts self.state().to_s()
-    
+
   end
-  
+
   def currentQuestion
     return self.state().current_node().questionText()
   end
-  
+
   def promptForYesNo
     loop do
       print " > "
@@ -45,7 +44,7 @@ class Game
       puts "please answer yes or no"
     end
   end
-  
+
   def advanceLeft()
     self.state().advanceTo(self.state().current_node().getLeftNodeFromDB(self.adb()))
   end
@@ -53,70 +52,67 @@ class Game
   def advanceRight()
     self.state().advanceTo(self.state().current_node().getRightNodeFromDB(self.adb()))
   end
-    
-  
-  
+
   def wordWithArticle(word)
- 
+
     if (word =~ /^[aeiou]/i)  # word starts with a vowel
-       article = "an"
-     else
+      article = "an"
+    else
       article = "a"
-     end
+    end
     return article + " " + word
   end
-  
 
   def get_new_question_for_node
     # need to prompt for a new question and then edit the tree
     # we'll end up with two new nodes, a question node and an animal node
-   
+
     lastAnimalName = self.state().current_node().text()
-    
+
     puts "I don't know what animal you're thinking of. Help me update my database."
     print "Please type in the name of the animal > "
 
     newAnimalName = gets.chomp()  # delete newline
-    
+
     print "You will need to type in a question that will distinguish between "
     print self.wordWithArticle(lastAnimalName)
     print " and "
     print self.wordWithArticle(newAnimalName) + ".\n"
-    
+
     puts "The question should be TRUE for one animal and FALSE for the other."
     puts "After you enter the question, I will ask for which animal the question is true."
     print "> "
 
     newQuestion = gets.chomp()
- 
+
     anAnimal = self.wordWithArticle(newAnimalName)
     print "\nIs this question true for " + anAnimal + "?"
-    
+
     trueForNewAnimal = self.promptForYesNo()
-    
+
     #theText = "Is it #{@anAnimal}?"
     #puts "New question is \"#{@theText}\""
     self.updateTree(newAnimalName,newQuestion,trueForNewAnimal)
-       
+
   end
-  
+
   def updateTree(newAnimalName,newQuestion,trueForNewAnimal)
     newNodeId = self.adb().next_node_id()
-    
+
     last_animal_node = self.state().current_node()
     last_question_node = self.state().parent_node()
-    
+
     new_animal_node = LeafNode.new()
     newAnimalId = self.adb().next_node_id()
     new_animal_node.id=(newAnimalId)
     new_animal_node.text=(newAnimalName)
-    
+
     new_question_node = BranchNode.new()
     newQuestionId = self.adb().next_node_id()
     new_question_node.id=(newQuestionId)
     new_question_node.text=(newQuestion)
-    
-#    puts "trueForNewAnimal = #{trueForNewAnimal}"
+
+    #    puts "trueForNewAnimal = #{trueForNewAnimal}"
     if (trueForNewAnimal)
       new_question_node.left_id=(newAnimalId)
       new_question_node.right_id=(last_animal_node.id())
@@ -124,17 +120,17 @@ class Game
       new_question_node.left_id=(last_animal_node.id())
       new_question_node.right_id=(newAnimalId)
     end
-    
+
     self.updateExistingNodeWith(last_question_node,last_animal_node,new_question_node)
- 
+
     # now persist the updates
-  
+
     new_animal_node.createInDB(self.adb())
     new_question_node.createInDB(self.adb())
     last_question_node.updateInDB(self.adb())
 
   end
-    
+
   def updateExistingNodeWith(parentNode,lastNode,newQuestionNode)
     if (lastNode.id() == parentNode.left_id())
       parentNode.left_id=(newQuestionNode.id())
@@ -143,46 +139,52 @@ class Game
     else
       # exception
     end
-    
+
   end
-  
+
 end
 
-loop do
-  game = Game.new()
-  game.initializeGame()
+class Player
+  def run
 
-  puts "Starting the Animal game"
-  finishedGame = false
-  
-  while (!finishedGame) do
-    print game.currentQuestion() + "? "
+    keepGoing = true
+    while (keepGoing) do
+      game = Game.new()
+      game.initializeGame()
 
-    answeredYes = game.promptForYesNo()
-    
-    if answeredYes
-      if (game.state().isLeaf())  # yea!  we're done!
-        puts "I guessed correctly. I must be very smart."
-        finishedGame = true
-      else
-        game.advanceLeft()
+      puts "Starting the Animal game"
+      finishedGame = false
+
+      while (!finishedGame) do
+        print game.currentQuestion() + "? "
+
+        answeredYes = game.promptForYesNo()
+
+        if answeredYes
+          if (game.state().isLeaf())  # yea!  we're done!
+            puts "I guessed correctly. I must be very smart."
+            finishedGame = true
+          else
+            game.advanceLeft()
+          end
+        else  # player answered no to question
+          if (!game.state().isLeaf()) # not at a leaf means we're at a branch
+            game.advanceRight()
+
+          else  # uh-oh.  got to the end of the questions and did not find the animal
+            game.get_new_question_for_node()
+            finishedGame = true
+
+          end
+        end
       end
-    else  # player answered no to question
-      if (!game.state().isLeaf()) # not at a leaf means we're at a branch
-        game.advanceRight()
 
-      else  # uh-oh.  got to the end of the questions and did not find the animal
-        game.get_new_question_for_node()
-        finishedGame = true
+      print "do you want to play again? "
+      keepGoing = game.promptForYesNo()
 
-      end
     end
   end
-
-  print "do you want to play again? "
-  keepGoing = game.promptForYesNo()
-  if (!keepGoing)
-    exit
-  end
-
 end
+
+p = Player.new()
+p.run()
